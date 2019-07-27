78f8ea2b683241ca915fd8f63f123d263f9265b8
Md. Rayhan Uddin Chowdhury <30935802+Rayhanuc@users.noreply.github.com>
2019-07-27 19:06:44 +0600

    some updated of flywheel

---
+++ app/sql/local.wp_users.sql
@@ -0,0 +1,5 @@
+/*!40101 SET NAMES binary*/;
+/*!40014 SET FOREIGN_KEY_CHECKS=0*/;
+/*!40103 SET TIME_ZONE='+00:00' */;
+INSERT INTO `wp_users` VALUES
+(1,"rayhan","$P$BmNNggNR0EI/2VSS/Ew9YYz8dbeEls.","rayhan","dev-email@flywheel.local","","2019-07-04 11:11:18","",0,"rayhan");
